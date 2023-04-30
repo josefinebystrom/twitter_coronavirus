6182e70cac3b6ef932b6aef9c4982a23e7a56aef
Josefine Bystrom <bystromjosefine@gmail.com>
2023-04-30 15:33:22 -0700

    Testing plot1

--- src/map.py
+++ src/map.py
@@ -37,6 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
@@ -60,10 +61,18 @@ with zipfile.ZipFile(args.input_path) as archive:
                 # search hashtags
                 for hashtag in hashtags:
                     lang = tweet['lang']
+                    try:
+                        country = tweet['place']['country_code']
+                    except:
+                        country = 'UNK'
+                    if country == '':
+                        country = 'UNK'
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
+                        counter_country[hashtag][country] += 1
+                    
+                    counter_country['_all'][country] += 1
                     counter_lang['_all'][lang] += 1
-
 # open the outputfile
 try:
     os.makedirs(args.output_folder)
@@ -72,7 +81,9 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
+output_path_country= output_path_base+'.country'
 print('saving',output_path_lang)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
-
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
--- src/visualize.py
+++ src/visualize.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import matplotlib
+matplotlib.use('Agg')
 
 # command line args
 import argparse
@@ -12,6 +14,8 @@ args = parser.parse_args()
 import os
 import json
 from collections import Counter,defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
 
 # open the input path
 with open(args.input_path) as f:
@@ -26,3 +30,22 @@ if args.percent:
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
 for k,v in items:
     print(k,':',v)
+
+#create bar graph
+keys = [item[0] for item in items[:10]]
+values = [item[1] for item in items[:10]]
+keys.reverse()
+values.reverse()
+plt.bar(sorted(keys), values)
+plt.ylabel("Number of Tweets")
+if args.input_path[-1] == 'g':
+    plt.title("The Top 10 Languages that tweeted" + args.key[1:] + "in 2020")
+    plt.xlabel("Language")
+    plt.savefig(args.key[1:] + '_lang.png')
+elif args.input_path == 'country':
+    plt.title("The Top 10 Countries that tweeted" + args.key[1:] + "in 2020")
+    plt.xlabel("Country")
+    plt.savefig(args.key[1:] + '_country.png')
+else:
+    plt.xlabel("Unknown Key")
+    plt.savefig(args.key[1:] + '_unknown.png')
